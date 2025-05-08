95f4ef1acdf4befd5bfbaffafe85c11ec47ec257
Jack Berman <jackberman@ufl.edu>
2025-05-08 14:35:02 -0400

    changed file names and created placeholder menu for users

---
+++ camera_funcs.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+from pco import Camera
+from constants import EXPOSURE_TIMES
+import numpy as np 
+
---
+++ main.py
@@ -0,0 +1,14 @@
+from pco import Camera
+from constants import EXPOSURE_TIMES
+import numpy as np 
+import matplotlib.pyplot as plt
+
+print("Welcome to the IMPS-4 Control Center!\n")
+print("--- CMOS Functions ---")
+print("1. Line Lamp Calibration")
+print("2. Polarization State Calibration")
+print("3. Live View")
+print("4. Exit")
+
+
+
