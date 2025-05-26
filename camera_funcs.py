2cb2d2dc159b1194d545eb646d97aee11ef874bb
Jack Berman <jackberman@ufl.edu>
2025-05-25 22:30:53 -0400

    created live view for camera using ring buffer mode, still need to test it. switched from matplotlib to cv2 to display image.

--- camera_funcs.py
+++ camera_funcs.py
@@ -2,4 +2,28 @@ import matplotlib.pyplot as plt
 from pco import Camera
 from constants import EXPOSURE_TIMES
 import numpy as np 
+import cv2
 
+def live_view(camera, exposure_time):
+
+    with Camera() as camera:
+        camera.record( # Start recording images
+            number_of_images=0,
+            exposure_time=exposure_time,
+            mode='ring buffer'
+        )
+
+        while True:
+            camera.wait_for_new_image()  # required for live view
+            image, meta = camera.image() # capture image
+
+            cv2.imshow("Captured Image", image) # use cv2 module to display the image
+            if cv2.waitKey(1) & 0xFF == ord('q'): # press 'q' to exit
+                camera.stop()
+                break
+
+    cv2.destroyAllWindows()
+
+
+
+    
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import camera_funcs
 from pco import Camera
-from constants import EXPOSURE_TIMES
-import numpy as np 
-import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+camera = Camera()
+
+
+
 
 print("Welcome to the IMPS-4 Control Center!\n")
 print("--- CMOS Functions ---")
-print("1. Line Lamp Calibration")
-print("2. Polarization State Calibration")
+print("1. Capture Image")
 print("3. Live View")
 print("4. Exit")
 
 
-
+camera_funcs.live_view(camera, exposure_time=1000)  # Example exposure time in ms
