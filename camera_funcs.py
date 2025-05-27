20a7a2ef62af9c6362654743885ee41eb6fd81fc
Jack Berman <jackberman@ufl.edu>
2025-05-27 10:20:05 -0400

    finished live_view mode, created capture image function with ability to do single capture, mulit capture, and average capture. planning on only doing average.

--- camera_funcs.py
+++ camera_funcs.py
@@ -3,9 +3,13 @@ from pco import Camera
 from constants import EXPOSURE_TIMES
 import numpy as np 
 import cv2
+import glob
+import os
+import time
+from PIL import Image
 
+# Live view function to display camera feed in real-time
 def live_view(camera, exposure_time):
-
     with Camera() as camera:
         camera.record( # Start recording images
             number_of_images=0,
@@ -23,6 +27,49 @@ def live_view(camera, exposure_time):
                 break
 
     cv2.destroyAllWindows()
+
+# Single exposure image capture function
+def capture_image(camera, exposure_time, num_images, average, file_path):
+    images = []
+    # checks if file path exists, if not, create it
+    if not os.path.exists(file_path):
+        os.mkdir(file_path)
+        
+    with Camera() as camera:
+        camera.record(
+            number_of_images=num_images,
+            exposure_time=exposure_time,
+            mode='sequence',
+            file_path=file_path
+        )
+    # wait for the camera to finish recording images
+    while camera.recorded_image_count != num_images:
+        time.sleep(0.001)
+
+    camera.stop()
+
+    # if average booolen is true, average the images
+    if (average):
+        image = camera.image_average()
+        images.add(image)
+    else:
+        for i in range(num_images):
+            image, meta = camera. image(image_index=i)
+            images.append(image)
+    # save the images to the file path
+    for i in range(num_images):
+        custom_name = f"my_custom_name_{i+1:03d}.tif"
+        Image.fromarray(img).save(os.path.join(filepath, custom_name))
+
+    cv2.imshow("Captured Image", images[-1]) # use cv2 module to display the image
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+
+       
+
 
 
 
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import camera_funcs
-from pco import Camera
-from PyQt5.QtWidgets import QApplication, QMainWindow
+# from pco import Camera
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
 
-camera = Camera()
+# GUI 
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+window.setGeometry(100, 100, 800, 600)
+window.setWindowTitle("IMPS-4 Control Center")
+window.show()
+sys.exit(app.exec_())
+
+
+# camera = Camera()
 
 
 
 
-print("Welcome to the IMPS-4 Control Center!\n")
-print("--- CMOS Functions ---")
-print("1. Capture Image")
-print("3. Live View")
-print("4. Exit")
+# print("Welcome to the IMPS-4 Control Center!\n")
+# print("--- CMOS Functions ---")
+# print("1. Capture Image")
+# print("3. Live View")
+# print("4. Exit")
 
 
-camera_funcs.live_view(camera, exposure_time=1000)  # Example exposure time in ms
+# camera_funcs.live_view(camera, exposure_time=1000)  # Example exposure time in ms
